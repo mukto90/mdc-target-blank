94b058610463eaafb586bffacfdbbeb2df8b0c19
Nazmul Ahsan <mukto90@MedhabiDotCom>
2016-02-12 13:40:14 +0600

    new version

--- mdc-target-blank.php
+++ mdc-target-blank.php
@@ -3,16 +3,15 @@
 	Plugin Name: MDC Target Blank
 	Description: MDC Target Blank forces links in posts or pages to be opened in a new tab. You don't have to write <strong>target="_blank"</strong> manually! Cheers..
 	Author: Nazmul Ahsan
-	Author URI: http://mukto.medhabi.com
-	Plugin URI: http://mukto.medhabi.com
-	Version: 1.0.0
+	Author URI: http://nazmulahsan.me
+	Plugin URI: https://wordpress.org/plugins/mdc-target-blank/
+	Version: 1.2
 	Tags: new tab, link new tab, target _blank, target blank, external link, open in new tab, mdc
 */
 function mdc_target_blank($content) {
-	global $post;
-	$post_string = get_the_content();
+	$post_string = $content;
 	$post_string = str_replace('<a', '<a target="_blank"', $post_string);
-  echo "<p>".$post_string."</p>";
+	return $post_string;
 }
 
 add_filter( 'the_content', 'mdc_target_blank' );
